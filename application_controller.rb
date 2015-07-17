18162951b9b4157f6ce170eff3f44c4ff83dbc0c
snadkarni18 <snadkarni@austinprep.org>
2015-07-17 13:38:06 +0000

    website

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,44 @@ Bundler.require
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    erb :index #Sana's comment
+    erb:index #Sana's comment
   end
-
+  
+  get '/selector.erb' do
+    erb:selector
+  end
+  
+  get '/survey' do
+    erb:survey
+  end
+  
+  get '/love.erb' do
+    erb:love
+  end
+  
+  get '/videogames.erb' do
+    erb:videogames
+  end
+  
+  get '/reading.erb' do
+    erb:reading
+  end
+ 
+  get '/sleeping.erb' do
+    erb:sleeping
+  end
+  
+   get '/walking.erb' do
+    erb:walking
+  end
+  
+  get '/walking.erb' do
+    erb:walking
+  end
+  
+   get '/programming.erb' do
+    erb:programming
+  end
+  
+  
 end
